b82fb16e7b75574ffa62bdc73aac1219a87f796f
Joshua Kurdys <jkurdys@gmail.com>
2022-03-09 06:23:36 -0700

    Completed Occupations challenge.

--- src/occupations.sql
+++ src/occupations.sql
@@ -1,12 +1,30 @@
-SELECT 'Employees' AS Employees,   
+/*
+Occupations
+
+Pivot the Occupation column in OCCUPATIONS so that each Name
+is sorted alphabetically and displayed underneath its
+corresponding Occupation. The output column headers should
+be Doctor, Professor, Singer, and Actor, respectively.
+
+Note: Print NULL when there are no more names corresponding
+to an occupation.
+
+Occupation will only contain one of the following values:
+Doctor, Professor, Singer or Actor.
+*/
+SELECT    
   [Doctor], [Professor], [Singer], [Actor]  
 FROM  
 (
-  SELECT Occupation, Name   
-  FROM OCCUPATIONS
-) AS SourceTable  
+  SELECT 
+    ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name) row_num, 
+    Name, 
+    Occupation
+    FROM OCCUPATIONS
+    GROUP BY Occupation, Name
+) AS SourceTable 
 PIVOT  
 (  
-  MIN(Name)  
+  MAX(Name)  
   FOR Occupation IN ([Doctor], [Professor], [Singer], [Actor])  
 ) AS PivotTable;
